7089bd759368da2fb397de06ead74cb26d534c39
angeltoast <elizabeth@itsliz.net>
2017-11-25 15:40:06 +0000

    new file:   firstrun.sh

---
+++ firstrun.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+# Developed by Elizabeth Mills - liz@feliz.one
+# Revision date: 25th November 2017
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+#      WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#            General Public License for more details.
+
+# A copy of the GNU General Public License is available from the Feliz2
+#        page at http://sourceforge.net/projects/feliz2/files
+#        or https://github.com/angeltoast/feliz2, or write to:
+#                 The Free Software Foundation, Inc.
+#                  51 Franklin Street, Fifth Floor
+#                    Boston, MA 02110-1301 USA
+
+# Functions in this module are used only once, on first use of FlashUSB
+# The main flash.sh script checks to see if Yad is installed
+# If not, function Dial is called from this module to install it.
+# After Yad is installed, this module is deleted
+
+function Dial
+{
+  while true
+  do
+    print_heading
+    echo "  FlasUSB needs the 'Yad' program installed, in order to display"
+    echo "  correctly. You should find it in your distro's package manager."
+    echo "  However, if you wish, it may be possible to install it now."
+    read -p "  Would you like to try? (y/N): " Response
+    case $Response in
+    "y" | "Y") # First make a reasoned guess of the install command based on distro
+      distro="${Distro,,}"                # Convert to all lower case for matching
+      case $distro in                     # Try to prepare install command
+      "arch*" | "antergos" | "Manjaro") Installer="pacman -S"
+        Updater="pacman -Syu"
+      ;;
+      "centos" | "red hat") Installer="yum install"
+        Updater="yum -y update && yum -y upgrade"
+      ;;
+      "debian" | "ubuntu" | "knoppix" | "*mint" | "sparky*") Installer="apt-get install"
+        Updater="apt-get update && apt-get upgrade"
+      ;;
+      "fedora") Installer="dnf install"
+        Updater="dnf update"
+      ;;
+      "mageia") Installer="urpmi"
+        Updater="urpmi --auto-update"
+      ;;
+      "suse" | "opensuse") Installer="zypper install"
+        Updater="zypper update"
+      ;;
+      *) Installer=""
+        Updater=""
+      esac
+      print_heading
+      if [ "$Installer" = "" ]; then
+        echo "   FlashUSB has been unable to determine the install command for your system."
+        echo "        This may be something like 'apt-get install' or 'yum install'"
+        echo "           or 'rpm -i' or 'pacman -S' - depending on your system."
+        echo "  If in doubt, you should not proceed (press [Enter] without typing anything)"
+        echo
+        read -p "  Please enter the installation command to use: " Installer
+        if [ "$Installer" = "" ]; then
+          echo
+          echo "Sorry to see you go"
+          exit
+        fi
+      fi
+      echo "      It appears that your system is $Distro"
+      echo "     In which case, your installation command"
+      echo "              is probably: $Installer"
+      echo
+      echo "    Would you like to proceed with installation"
+      echo "              using: $Installer?"
+      echo
+      echo "  Please choose:"
+      echo "   1 to install using using: ${Installer};"
+      echo "   2 for an opportunity to enter a different install command;"
+      echo "   3 to quit FlashUSB"
+      echo
+      read -p "   Enter a number: " Response
+      case $Response in
+        1) gksu "${Installer}" dialog
+          break
+        ;;
+        2) print_heading
+          read -p "  Please enter the installation command to use: " Installer
+          if [ "$Installer" = "" ]; then
+            echo; echo "Sorry to see you go"; echo
+            exit
+          fi
+          echo; echo "  Would you like to proceed with installation"; echo
+          read -p "  using: $Installer? (y/N): " Response
+          case $Response in
+            "y" | "Y") gksu "${Installer}" yad
+              break
+            ;;
+            *) echo; echo "Sorry to see you go"; echo
+              exit
+          esac
+        ;;
+        3) echo; echo "Sorry to see you go"; echo
+            exit
+        ;;
+        *) echo "  Invalid entry. Please try again."
+          read -p "  Press [Enter] to continue"
+      esac
+    ;;
+    "n" | "N" | "") echo; echo "  Sorry to see you go"; echo
+        exit
+    ;;
+    *) echo; echo "  Invalid entry. Please try again."
+      read -p "  Press [Enter] to continue"
+    esac
+  done
+  
+} # Dial
+
+function print_heading                   # Always use this function to clear the screen in text mode
+{
+  clear
+  T_COLS=$(tput cols)                    # Get width of terminal
+  LenBT=${#Backtitle}                    # Length of backtitle
+  HalfBT=$((LenBT/2))
+  tput cup 0 $(((T_COLS/2)-HalfBT))      # Move the cursor to left of center
+  tput bold
+  printf "%-s\n" "$Backtitle"            # Display backtitle
+  tput sgr0
+  cursor_row=3                           # Save cursor row after heading
+} # print_heading
